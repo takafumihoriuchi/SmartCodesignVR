69ee64cbc70bc0053c74c78536bf1e45f34f26a2
Takafumi Horiuchi <hawkletter080e@gmail.com>
2020-11-24 18:42:47 +0900

    followed oculus unity tutorial webpage and made a simple rolling ball demo scene.

---
+++ Assets/Scripts/ColorControllerTutorial.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorControllerTutorial : MonoBehaviour
+{
+	public Material[] wallMaterial;
+	Renderer rend;
+
+	public Text displayText;
+
+    void Start()
+    {
+		rend = GetComponent<Renderer>();
+		rend.enabled = true;
+		displayText.text = "";
+    }
+
+    private void OnCollisionEnter(Collision col) {
+    	if (col.gameObject.name == "player-ball") {
+    		displayText.text = "Collision detected!";
+    		rend.sharedMaterial = wallMaterial[0];
+    	}
+    }
+
+    private void OnCollisionExit(Collision col) {
+    	if (col.gameObject.name == "player-ball") {
+    		rend.sharedMaterial = wallMaterial[1];
+    		displayText.text = "No collision detected.";
+    	}
+    }
+
+    void Update() {}
+}
