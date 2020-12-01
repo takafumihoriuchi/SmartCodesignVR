5582841cf205bd1750ae85afb05075ae7c66ad07
Takafumi Horiuchi <hawkletter080e@gmail.com>
2020-12-01 16:46:47 +0900

    implemented reset button

---
+++ Assets/Scripts/ButtonLimit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLimit : MonoBehaviour
+{
+	public GameObject buttonTrigger;
+	private Vector3 originalPosition;
+
+    void Start()
+    {
+        originalPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (transform.position.z >= originalPosition.z) {
+        	transform.position = originalPosition;
+        }
+        if (transform.position.z < buttonTrigger.transform.position.z) {
+        	transform.position = new Vector3(transform.position.x, transform.position.y, buttonTrigger.transform.position.z);
+        }
+        // if (Vector3.Distance(buttonTrigger.transform.position, transform.position) == 0) {
+        if (transform.position.z == buttonTrigger.transform.position.z) {
+        	Debug.Log("reloading scene...");
+        	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour
+{
+	private bool pressInProgress;
+
+	private void OnTriggerEnter(Collider col) {
+		if ((col.tag == "ButtonActivator") && !pressInProgress) {
+			pressInProgress = true;
+		}
+	}
+
+	private void OnTriggerExit(Collider col) {
+		if (col.tag == "ButtonActivator") {
+			pressInProgress = false;
+		}
+	}
+
+}
