f61d419779122625413e1b0ca2d4788d22caaf6f
Takafumi Horiuchi <hawkletter080e@gmail.com>
2020-12-03 02:34:41 +0900

    completed interactoin-flow of one exmaple of smart object

--- Assets/Scripts/TrashcanEffects.cs
+++ Assets/Scripts/TrashcanEffects.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class TrashcanEffects : MonoBehaviour
 {
-	AudioSource alert_source;
+	private GameObject TrashBin;
+	private AudioSource alert_sound;
+	// public Material[] mat;
+	// private Renderer rend;
 
     void Start() {
-    	alert_source = GameObject.Find("trashBin").GetComponent<AudioSource>();
+    	TrashBin = GameObject.Find("trashBin");
+    	alert_sound = TrashBin.GetComponent<AudioSource>();
+    	// rend = TrashBin.GetComponent<Renderer>();
+    	// rend.enabled = true;
+    	// rend.shareMaterial = mat[0];
     }
 
     void Update() {
     	
     }
 
-    void OnCollisionEnter(Collision collision) {
-		if (collision.gameObject.name.Equals("ground-plane"))
-			if (!alert_source.isPlaying) alert_source.Play();
+    void OnCollisionEnter(Collision col) {
+		if (col.gameObject.name.Equals("ground-plane")) {
+			if (!alert_sound.isPlaying) {
+				alert_sound.Play();
+				// rend.shareMaterial = material;
+			}
+		}
 	}
 
 }
