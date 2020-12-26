2e8fc4560e684126d20190eac042e3d8abd772ae
Takafumi Horiuchi <hawkletter080e@gmail.com>
2020-12-26 17:49:01 +0900

    now able to change color of environment object

---
+++ Assets/Scripts/ChangeMaterial.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour
+{
+	public Material[] material;
+	Renderer rend;
+
+    void Start() {
+        rend = transform.GetChild(0).gameObject.GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+
+    void OnCollisionEnter (Collision col) {
+    	Debug.Log("Entered OnCollisionEnter, " + col.gameObject.name);
+    	if (col.gameObject.name.Equals("RedPaint"))
+    		rend.sharedMaterial = material[1];
+    	else if (col.gameObject.name.Equals("GreenPaint"))
+    		rend.sharedMaterial = material[2];
+    	else if (col.gameObject.name.Equals("BluePaint"))
+    		rend.sharedMaterial = material[3];
+    	else if (col.gameObject.name.Equals("YellowPaint"))
+    		rend.sharedMaterial = material[4];
+    	else if (col.gameObject.name.Equals("VioletPaint"))
+    		rend.sharedMaterial = material[5];
+    }
+
+    void Update() {}
+
+}
