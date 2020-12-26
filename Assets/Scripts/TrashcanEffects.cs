a5889c340b98d249669031ba94149e48fca942bb
Takafumi Horiuchi <hawkletter080e@gmail.com>
2020-12-26 12:08:00 +0900

    cleaned files for fresh start

--- Assets/Scripts/TrashcanEffects.cs
+++ Assets/Scripts/TrashcanEffects.cs
@@ -6,15 +6,10 @@ public class TrashcanEffects : MonoBehaviour
 {
 	private GameObject TrashBin;
 	private AudioSource alert_sound;
-	// public Material[] mat;
-	// private Renderer rend;
 
     void Start() {
     	TrashBin = GameObject.Find("trashBin");
     	alert_sound = TrashBin.GetComponent<AudioSource>();
-    	// rend = TrashBin.GetComponent<Renderer>();
-    	// rend.enabled = true;
-    	// rend.shareMaterial = mat[0];
     }
 
     void Update() {    	
@@ -22,10 +17,7 @@ public class TrashcanEffects : MonoBehaviour
 
     void OnCollisionEnter(Collision col) {
 		if (col.gameObject.name.Equals("GroundPlane")) {
-			if (!alert_sound.isPlaying) {
-				alert_sound.Play();
-				// rend.shareMaterial = material;
-			}
+			if (!alert_sound.isPlaying) alert_sound.Play();
 		}
 	}
 
