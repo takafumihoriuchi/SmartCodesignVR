0fa6300f00c01016dfab28700056d720ed28f180
Takafumi Horiuchi <hawkletter080e@gmail.com>
2020-12-29 22:13:16 +0900

    enabled drawing in space with array

--- Assets/Scripts/MotionRecorder.cs
+++ Assets/Scripts/MotionRecorder.cs
@@ -5,27 +5,51 @@ using UnityEngine;
 public class MotionRecorder : MonoBehaviour
 {
 	private bool recording;
+	private bool allowRecording;
+
+ 	public GameObject parentOfTrail;
+ 	public TrailRenderer originalTrail;
+ 	private TrailRenderer[] trailArr;
+ 	private int sizeOfArr = 100;
+ 	private int trailIndex;
+ 	// TODO: change from array to other "sustainable" data structure (e.g. linked list)
+ 	// must be able to delete as well; specific instance when designated (TrailRenderer.Clear)
 
     void Start() {
-		recording = false;        
+		allowRecording = true;
+		recording = false;
+		trailArr = new TrailRenderer[sizeOfArr];
+		trailIndex = 0;
+		trailArr[trailIndex] = originalTrail;
+		trailArr[trailIndex].emitting = false;
+		trailIndex++;
     }
 
     void Update() {
-		if (!recording) {
-			if (OVRInput.GetDown(OVRInput.RawButton.B) || OVRInput.GetDown(OVRInput.RawButton.Y)) {
+		if (allowRecording) {
+			if (recording == false && (OVRInput.GetDown(OVRInput.RawButton.B) || OVRInput.GetDown(OVRInput.RawButton.Y))) {
 				recording = true;
-				Debug.Log("recording started");
-			}
-		} else {
-			if (OVRInput.GetDown(OVRInput.RawButton.B) || OVRInput.GetDown(OVRInput.RawButton.Y)) {
+				Debug.Log("RECORDING IN PROGRESS...");
+				trailArr[trailIndex] = Instantiate(originalTrail, parentOfTrail.transform.position, Quaternion.identity, parentOfTrail.transform) as TrailRenderer;
+				trailArr[trailIndex].emitting = true;
+			} else if (recording == true && (OVRInput.GetUp(OVRInput.RawButton.B) || OVRInput.GetUp(OVRInput.RawButton.Y))) {
 				recording = false;
-				Debug.Log("recording ended");	
+				Debug.Log("RECORDING END");
+				trailArr[trailIndex].emitting = false;
+				trailIndex++;
 			}
 		}
 
     }
 
+
 }
+
+/*
+B|Yボタンを押しているときは、新しい線を描画できるようにしたい。
+話している時は、描画できなくしたい。
+
+*/
 
 // boolean of button pressed / release
 // GetDown => true when pressed
