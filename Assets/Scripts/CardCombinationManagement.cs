1c661a2fbf5ab003da438163ce99d245f637daff
Takafumi Horiuchi <hawkletter080e@gmail.com>
2021-01-02 14:15:25 +0900

    implemented voice guide and scene transition script

--- Assets/Scripts/CardCombinationManagement.cs
+++ Assets/Scripts/CardCombinationManagement.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CardCombinationManagement : MonoBehaviour
 {
-    void Start() {
+	public AudioSource introVoice;
+	public AudioSource diveVoice;
 
+	private bool comboComp;
+    private bool playedDiveVoice;
+
+    void Start() {
+    	comboComp = false;
+    	playedDiveVoice = false;
+    	introVoice.PlayDelayed(3);
+    	// TODO: better to wait for the user to wear the HMD (e.g. play when user comes near the boxes)
     }
 
     void Update() {
-        if (SelectionDetectionEnv.envSelected && SelectionDetectionIn.inSelected && SelectionDetectionOut.outSelected) {
-        	Debug.Log("three cards have been selected; [env, in, out] = [" + SelectionDetectionEnv.envSelectedObject.name + ", " + SelectionDetectionIn.inSelectedObject.name + ", " + SelectionDetectionOut.outSelectedObject.name + "]");
+        // update card completion status every frame (TODO: improve computational efficiency)
+        comboComp = SelectionDetectionEnv.envSelected
+                 && SelectionDetectionIn.inSelected
+                 && SelectionDetectionOut.outSelected;
+        if (comboComp) {
+        	if (!diveVoice.isPlaying && !playedDiveVoice) { // hasn't played voice yet
+        		if (introVoice.isPlaying) introVoice.Stop();
+        		diveVoice.Play(0);
+        		playedDiveVoice = true;
+			}
+			if (!diveVoice.isPlaying && playedDiveVoice) { // has already finished playing voice
+				Debug.Log("Moving on to the next scene.");
+				Debug.Log("[env, in, out] = [" + SelectionDetectionEnv.envSelectedObject.name + ", " + SelectionDetectionIn.inSelectedObject.name + ", " + SelectionDetectionOut.outSelectedObject.name + "]");
+				SceneManager.LoadScene("PrototypingScene");
+			}
+        } else {
+        	if (diveVoice.isPlaying) {
+        		diveVoice.Stop();
+        		playedDiveVoice = false;
+        	}
         }
     }
 
