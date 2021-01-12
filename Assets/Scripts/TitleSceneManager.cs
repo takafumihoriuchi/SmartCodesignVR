f8d505c844e60c69b133aa0ce1190e8ed7f41dad
Takafumi Horiuchi <hawkletter080e@gmail.com>
2021-01-12 17:52:09 +0900

    added Title Scene (a scene to wait for the player to wear the HMD; they only have to press A-button in this scene)

---
+++ Assets/Scripts/TitleSceneManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : MonoBehaviour
+{
+	[SerializeField] OVRScreenFade fade;
+
+	public SpriteRenderer redHighlightImage;
+	private WaitForSeconds _oneSec = new WaitForSeconds(1f);
+	private Color _spriteColor;
+
+	AudioSource clickSound;
+
+    void Start()
+    {
+        _spriteColor = redHighlightImage.color;
+        _spriteColor.a = 0f;
+        StartCoroutine(BlinkContollerImage());
+
+        clickSound = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.RawButton.A)
+        	|| OVRInput.GetDown(OVRInput.RawButton.X) ||Input.GetKeyDown("space") ) {
+			clickSound.Play();
+			fade.fadeColor = Color.white;
+			fade.FadeOut();
+			SceneManager.LoadScene(1, LoadSceneMode.Single);
+		}
+    }
+
+    IEnumerator BlinkContollerImage() {
+    	while (true) {
+    		_spriteColor.a = 1f - _spriteColor.a;
+    		redHighlightImage.color = _spriteColor;
+    		yield return _oneSec;
+    	}
+    }
+
+}
