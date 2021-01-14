e2b3d03193b5b748dd2d1cb5aa94d716e194640e
Takafumi Horiuchi <hawkletter080e@gmail.com>
2021-01-15 01:15:07 +0900

    added microphone model

--- Assets/myScripts/ScriptsPrototypingScene/OutputMakeSound.cs
+++ Assets/myScripts/ScriptsPrototypingScene/OutputMakeSound.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class OutputMakeSound : MonoBehaviour
 {
-    public Button recordButton;
     private bool isRecording;
     private AudioSource soundRecorder;
+    [SerializeField] private Button recordButton;
+    [SerializeField] private TextMeshProUGUI whenDescription;
+    private int tmpRecordNum;
+    private bool tmpRecordingSaved;
 
     void Start() {
         isRecording = false;
+        tmpRecordingSaved = false;
         recordButton.onClick.AddListener(RecordOutputSound);
+        tmpRecordNum = 0;
     }
 
     void Update() {
@@ -19,18 +25,21 @@ public class OutputMakeSound : MonoBehaviour
 
     void RecordOutputSound()
     {
-        if (!isRecording)
-        {
-            soundRecorder = GetComponent<AudioSource>();
-            //AudioSource.clip = Microphone.Start("Android audio input",false,60,16000);
-            soundRecorder.clip = Microphone.Start("",false,60,16000);
-            isRecording = true;
-        } else
-        {
-            //soundRecorder.clip = Microphone.End("Android audio input");
+        if (!isRecording) {
+            if (!tmpRecordingSaved) {
+                soundRecorder = GetComponent<AudioSource>();
+                soundRecorder.clip = Microphone.Start("", false, 60, 16000);
+                isRecording = true;
+                tmpRecordNum++;
+                whenDescription.text = "Play [recording in process...] (click again to stop&finalize)";
+            } else {
+                soundRecorder.Play();
+            }
+        } else { // TODO: enable re-recording of sound
             Microphone.End("");
+            whenDescription.text = "Play Record#" + tmpRecordNum;
             isRecording = false;
-            soundRecorder.Play();
+            tmpRecordingSaved = true;
         }
 
     }
