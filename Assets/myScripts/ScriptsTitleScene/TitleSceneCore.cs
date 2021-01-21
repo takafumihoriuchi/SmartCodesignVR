e594710ff9d9d3a7b968f2ff9ba13e25f3db1231
Takafumi Horiuchi <hawkletter080e@gmail.com>
2021-01-21 21:39:50 +0900

    refactored TitleScene

---
+++ Assets/myScripts/ScriptsTitleScene/TitleSceneCore.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneCore : MonoBehaviour
+{
+	public OVRScreenFade screenFade;
+	public SpriteRenderer buttonRedHighlightImage;
+	private WaitForSeconds oneSec = new WaitForSeconds(1f);
+	private Color spriteColor;
+	private AudioSource clickSound;
+
+    void Start()
+    {
+        clickSound = GetComponent<AudioSource>();
+
+        spriteColor = buttonRedHighlightImage.color;
+        spriteColor.a = 0f;
+        StartCoroutine(BlinkContollerImage());
+    }
+    
+    void Update()
+    {
+        // accept keyboard input for development purpose
+        if (OVRInput.GetDown(OVRInput.RawButton.A) || Input.GetKeyDown(KeyCode.A)) {
+			clickSound.Play();
+			screenFade.fadeColor = Color.white;
+			screenFade.FadeOut();
+			SceneManager.LoadScene(1, LoadSceneMode.Single);
+		}
+    }
+
+    IEnumerator BlinkContollerImage() {
+    	while (true) {
+    		spriteColor.a = 1f - spriteColor.a;
+            buttonRedHighlightImage.color = spriteColor;
+    		yield return oneSec;
+    	}
+    }
+
+}
